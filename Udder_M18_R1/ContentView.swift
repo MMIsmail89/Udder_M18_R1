2cabde97c8dc70629655c17c49926fc2002905e2
zeyadBadawy <zeyad.badawey@gmail.com>
2025-04-23 13:17:42 +0300

    Initial Commit

---
+++ Udder_M18_R1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Udder_M18_R1
+//
+//  Created by zeyad on 23/04/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
